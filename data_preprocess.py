70abfbb248a44ed8959440451438a579a6b5c7f8
Christopher Chiu <50348744+christopher-chiu@users.noreply.github.com>
2021-07-29 21:19:12 -0400

    clean comments and refactor a bit

--- data_preprocess.py
+++ data_preprocess.py
@@ -14,9 +14,6 @@ import numpy as np
 import cv2
 import os
 import glob
-import os.path as ops
-import random
-import tensorflow as tf
 import pickle
 
 def generateGroundTruths(labelList, dataFolder, imgFolder, gtFolder):
@@ -62,99 +59,7 @@ def generateGroundTruths(labelList, dataFolder, imgFolder, gtFolder):
             cv2.imwrite(gt_save_path, mask[:,:,1])
 
 
-#
-# def generate_index_files(dataFolder, imgFolder, gtFolder):
-#     index = []
-#
-#     for img in glob.glob(imgFolder + "/*.png"):
-#         img_name = ops.split(img)[1]
-#         binary_img =  gtFolder + img_name
-#
-#         index.append('{:s} {:s}\n'.format(
-#             imgFolder + img_name,
-#             binary_img)
-#         )
-#
-#     random.shuffle(index)
-#
-#     length = len(index)
-#     train_index = index[:int(length * 0.7)]
-#     val_index = index[int(length*0.7):int(length*0.85)]
-#     test_index = index[int(length*0.85):]
-#
-#     random.shuffle(train_index)
-#     random.shuffle(val_index)
-#     random.shuffle(test_index)
-#
-#     with open(dataFolder + 'train.txt', 'w') as file:
-#         file.write(''.join(train_index))
-#
-#     with open(dataFolder + 'test.txt', 'w') as file:
-#         file.write(''.join(test_index))
-#
-#     with open(dataFolder + 'val.txt', 'w') as file:
-#         file.write(''.join(val_index))
-#
-#
-#
-#
-# # read in the index files
-# def read_index(path):
-#     img_info = []
-#     gt_info = []
-#
-#     with open(path, 'r') as file:
-#         for line in file:
-#             parsed_line = line.rstrip('\r').rstrip('\n').split(' ')
-#             img_info.append(parsed_line[0])
-#             gt_info.append(parsed_line[1])
-#     return {'img_info': img_info, 'gt_info': gt_info}
-#
-#
-# def generate_tfrecords(gt_images_paths, gt_binary_images_paths, tfrecords_path):
-#
-#     RESIZE_IMAGE_HEIGHT = 80
-#     RESIZE_IMAGE_WIDTH = 160
-#
-#     _tfrecords_dir = ops.split(tfrecords_path)[0]
-#     os.makedirs(_tfrecords_dir, exist_ok=True)
-#
-#     with tf.compat.v1.python_io.TFRecordWriter(tfrecords_path) as _writer:
-#         for _index, _gt_image_path in enumerate(gt_images_paths):
-#
-#             # prepare gt image
-#             _gt_image = cv2.imread(_gt_image_path, cv2.IMREAD_UNCHANGED)
-#             if _gt_image.shape != (RESIZE_IMAGE_WIDTH, RESIZE_IMAGE_HEIGHT, 3):
-#                 _gt_image = cv2.resize(
-#                     _gt_image,
-#                     dsize=(RESIZE_IMAGE_WIDTH, RESIZE_IMAGE_HEIGHT),
-#                     interpolation=cv2.INTER_LINEAR
-#                 )
-#             _gt_image_raw = _gt_image.tostring()
-#
-#             # prepare gt binary image
-#             _gt_binary_image = cv2.imread(gt_binary_images_paths[_index], cv2.IMREAD_UNCHANGED)
-#             if _gt_binary_image.shape != (RESIZE_IMAGE_WIDTH, RESIZE_IMAGE_HEIGHT, 1):
-#                 _gt_binary_image = cv2.resize(
-#                     _gt_binary_image,
-#                     dsize=(RESIZE_IMAGE_WIDTH, RESIZE_IMAGE_HEIGHT),
-#                     interpolation=cv2.INTER_NEAREST
-#                 )
-#                 _gt_binary_image = np.reshape(_gt_binary_image, (RESIZE_IMAGE_WIDTH, RESIZE_IMAGE_HEIGHT, 1))
-#                 _gt_binary_image = np.array(_gt_binary_image / 255.0, dtype=np.uint8)
-#             _gt_binary_image_raw = _gt_binary_image.tostring()
-#
-#             _example = tf.train.Example(
-#                 features=tf.train.Features(
-#                     feature={
-#                         'gt_image_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[_gt_image_raw])),
-#                         'gt_binary_image_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[_gt_binary_image_raw]))
-#                     }))
-#             _writer.write(_example.SerializeToString())
-#
-#     return
-
-def pickleDataset(imgFolder, gtFolder):
+def pickleDataset(imgFolder, gtFolder, pickledImgs, pickledGt):
 
     all_imgs = []
     all_gt = []
@@ -187,9 +92,8 @@ def pickleDataset(imgFolder, gtFolder):
             gt = np.array(gt / 255.0, dtype=np.uint8)
         all_gt.append(gt)
 
-    pickle.dump(np.array(all_imgs), open("./data/full_CNN_train_2.p", "wb" ))
-    pickle.dump(np.array(all_gt), open("./data/full_CNN_labels_2.p", "wb"))
-
+    pickle.dump(np.array(all_imgs), open(pickledImgs, "wb"))
+    pickle.dump(np.array(all_gt), open(pickledGt, "wb"))
 
 
 if __name__ == '__main__':
@@ -201,35 +105,10 @@ if __name__ == '__main__':
     os.makedirs(imgFolder, exist_ok=True)  # create folder
     os.makedirs(gtFolder, exist_ok=True)  # create folder
 
-    # TODO - add in the remaining jsons once we can train the model
+    # Add in the json files here that you want the model to train on
     jsonList = ['label_data_0531.json']
     generateGroundTruths(jsonList, dataFolder, imgFolder, gtFolder)
 
-    pickleDataset(imgFolder, gtFolder)
-
-
-
-    '''
-    This code below here generates the tfrecord files for train/test/val
-    Since I switched over to use pickle for serialization, it's not necessary...
-    '''
-    # generate_index_files(dataFolder, imgFolder, gtFolder)
-    #
-    # tfRecordFolder = dataFolder + '/tfrecords/'
-    # os.makedirs(tfRecordFolder, exist_ok=True)
-    #
-    # train_info = read_index(dataFolder + 'train.txt')
-    # val_info = read_index(dataFolder + 'val.txt')
-    # test_info = read_index(dataFolder + 'test.txt')
-    #
-    # generate_tfrecords(train_info['img_info'],
-    #                    train_info['gt_info'],
-    #                    tfRecordFolder + 'tusimple_train.tfrecords')
-    #
-    # generate_tfrecords(val_info['img_info'],
-    #                    val_info['gt_info'],
-    #                    tfRecordFolder + 'tusimple_val.tfrecords')
-    #
-    # generate_tfrecords(test_info['img_info'],
-    #                    test_info['gt_info'],
-    #                    tfRecordFolder + 'tusimple_test.tfrecords')
+    pickledImgs = dataFolder + '/full_train.p'
+    pickledGt = dataFolder + '/full_labels.p'
+    pickleDataset(imgFolder, gtFolder, pickledImgs, pickledGt)
--- load_model.py
+++ load_model.py
@@ -1,7 +1,5 @@
-import os
 import cv2
 import tensorflow as tf
-from tensorflow import keras
 import numpy as np
 
 RESIZE_IMAGE_HEIGHT = 80
@@ -12,37 +10,39 @@ class Lanes():
       self.recent_fit = []
       self.avg_fit = []
 
-trained_model = tf.keras.models.load_model('full_CNN_model_2.h5')
-img = cv2.imread('./test_data/1494452385593783358/1.jpg')
-lanes = Lanes()
 
-resized_img = cv2.resize(img, dsize=(80, 160))
-resized_img = np.array(resized_img)
-resized_img = resized_img[None,:,:,:]
+if __name__ == '__main__':
+    trained_model = tf.keras.models.load_model('segnet_model_weights.h5')
+    img = cv2.imread('./test_data/1494452385593783358/1.jpg')
+    lanes = Lanes()
 
-# Make prediction with neural network (un-normalize value by multiplying by 255)
-prediction = trained_model.predict(resized_img)[0]
-prediction = np.where(prediction < 0.07, 0, prediction)
-prediction = np.ceil(prediction)
-prediction = prediction*255.0
+    resized_img = cv2.resize(img, dsize=(RESIZE_IMAGE_HEIGHT, RESIZE_IMAGE_WIDTH))
+    resized_img = np.array(resized_img)
+    resized_img = resized_img[None,:,:,:]
 
-lanes.recent_fit.append(prediction)
+    # Make prediction with neural network (un-normalize value by multiplying by 255)
+    prediction = trained_model.predict(resized_img)[0]
+    prediction = np.where(prediction < 0.07, 0, prediction) # use 0.07 as our threshold for the lane prediction
+    prediction = np.ceil(prediction)
+    prediction = prediction*255.0
 
-if len(lanes.recent_fit) > 5:
-    lanes.recent_fit = lanes.recent_fit[1:]
+    lanes.recent_fit.append(prediction)
 
-# Calculate average detection
-lanes.avg_fit = np.mean(np.array([i for i in lanes.recent_fit]), axis = 0)
+    if len(lanes.recent_fit) > 5:
+        lanes.recent_fit = lanes.recent_fit[1:]
 
-# Generate fake R & B color dimensions, stack with G
-blanks = np.zeros_like(lanes.avg_fit).astype(np.uint8)
-lane_drawn = np.dstack((blanks, lanes.avg_fit, blanks))
+    # Calculate average detection
+    lanes.avg_fit = np.mean(np.array([i for i in lanes.recent_fit]), axis = 0)
 
-# Re-size to match the original image
-lane_image = cv2.resize(lane_drawn, dsize = (1280, 720))
+    # Generate fake R & B color dimensions, stack with G
+    blanks = np.zeros_like(lanes.avg_fit).astype(np.uint8)
+    lane_drawn = np.dstack((blanks, lanes.avg_fit, blanks))
 
-# Merge the lane drawing onto the original image
-result = cv2.addWeighted(img, 0.5, lane_image, 0.5, 0, dtype = cv2.CV_32F)
+    # Re-size to match the original image
+    lane_image = cv2.resize(lane_drawn, dsize=(1280, 720))
+
+    # Merge the lane drawing onto the original image
+    result = cv2.addWeighted(img, 0.5, lane_image, 0.5, 0, dtype=cv2.CV_32F)
 
 
-cv2.imwrite('mask_img.jpg', result)
+    cv2.imwrite('mask_img.jpg', result)
